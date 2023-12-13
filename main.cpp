6dcc759b7872bbd7a3b1402801093514262ab766
Enrique Rodriguez <enrique_rodriguez743@aol.com>
2023-12-13 19:34:48 +0100

    Lösung für Teil 2 von Rätsel 1

--- main.cpp
+++ main.cpp
@@ -2,21 +2,45 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <functional>
+#include <stdint.h>
+
+struct FloorCounter {
+  int32_t floorCount { 0 };
+
+  bool basementReached { false };
+
+  uint32_t readSigns{ 0 };
+
+   const std::function<void(const char&)> calculate = [this](const char& sign) {
+      this->readSigns++;
+      if(sign == '(')
+        this->floorCount++;
+      else if(sign == ')')
+        this->floorCount--;
+      if(floorCount <= -1 and not(basementReached)) {
+        basementReached = true;
+        std::cout << "Im Erdgeschoss angekommen bei !" << readSigns;
+      } 
+    };     
+};
 
 int main() {
+
+  FloorCounter myCounter { };
+
   std::ifstream myfile {"../code.txt"};
   std::string line;
   
   if (myfile.is_open()) {
     std::getline(myfile, line);
     myfile.close();  // Close the file explicitly
-    size_t amountOfOpeningBrackets =  std::count(line.begin(), line.end(), '(');
-    size_t amountOfClosingBrackets = std::count(line.begin(), line.end(), ')');
-    std::cout << "Anzahl an Bracket-Diff: " << amountOfOpeningBrackets-amountOfClosingBrackets;
+    std::for_each(line.begin(), line.end(), [&](const char& sign) {
+      myCounter.calculate(sign);
+    });
   }
   else {
     std::cerr << "Datei konnte nicht geoeffnet werden !" << std::endl;
   }
-  
   return 0;
 }
