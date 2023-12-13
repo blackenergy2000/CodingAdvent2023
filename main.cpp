2f111e6029591356c3ccbca97a3d5adc2d91a3d4
Enrique Rodriguez <enrique_rodriguez743@aol.com>
2023-12-13 18:43:18 +0100

    Part 1, Tag 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
 
-int main() { 
-
+int main() {
+  std::ifstream myfile {"../code.txt"};
+  std::string line;
+  
+  if (myfile.is_open()) {
+    std::getline(myfile, line);
+    myfile.close();  // Close the file explicitly
+    size_t amountOfOpeningBrackets =  std::count(line.begin(), line.end(), '(');
+    size_t amountOfClosingBrackets = std::count(line.begin(), line.end(), ')');
+    std::cout << "Anzahl an Bracket-Diff: " << amountOfOpeningBrackets-amountOfClosingBrackets;
+  }
+  else {
+    std::cerr << "Datei konnte nicht geoeffnet werden !" << std::endl;
+  }
+  
   return 0;
 }
