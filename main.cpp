0c2af3f21dad6be8d8648c8fc7e39ebd8edfe7c1
Enrique <enrique_rodriguez743@aol.com>
2023-12-16 08:16:15 +0100

    Implementierung des Part2 für Tag 1 2023

--- main.cpp
+++ main.cpp
@@ -3,61 +3,109 @@
 #include <string>
 #include <algorithm>
 #include <functional>
-#include <stdint.h>
+#include <stdint.h> 
+#include <array>
+#include <optional>
 
-struct FloorCounter {
 
-  std::vector<uint8_t> numbers { };
+struct FloorCounter { 
 
-  unsigned total { 0 };
+  /*****************************************************************************************
+   * @brief Arrays that are used to store the minimal and maximal position at which the 
+   * numbers of the string are located. 
+  *****************************************************************************************/
+  std::array<size_t, 10> lowerPosition { };
+  std::array<size_t, 10> upperPosition { };  
 
-  FloorCounter() {
-    this->numbers.reserve(10);
-  }
 
-  const std::function<void(const char&)> saveSign = [this](const char& sign) {
-      if( isdigit(sign) ) {
-        this->numbers.push_back(static_cast<uint8_t>(sign)-48);
-      }
-  };     
+  FloorCounter() { }  
 };
 
+  /*****************************************************************************************
+   * @brief Array with all the strings that can be interpreted as numbers. 
+  *****************************************************************************************/
+  static const std::array<std::string_view, 10> charNumbers {
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+  }; 
+
+ /*****************************************************************************************
+   * @brief Array with all the numbers. 
+  *****************************************************************************************/
+  static const std::array<std::string, 10> numbers {
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
+  };
+
 int main() {
-
-  FloorCounter myCounter { };
-
+  size_t totalValue { 0 };
   std::ifstream myfile {"../code.txt"};
   std::string line;     
   
   if (myfile.is_open()) {
     while(not myfile.eof()) { 
       std::getline(myfile, line);
-        std::for_each(line.begin(), line.end(), [&](const char& sign) {
-        myCounter.saveSign(sign);
-      });
-      //Build the sum of the single digits
-      uint8_t digit { 0 };
-      switch(  myCounter.numbers.size() ) { 
-        case 0:
-          break;
-        case 1:
-          digit = myCounter.numbers.at(0);
-          myCounter.total += static_cast<unsigned>(digit*10 + digit);
-          break;
-        default:
-          if( myCounter.numbers.size() >= 2) { 
-            myCounter.total += static_cast<unsigned>(myCounter.numbers.front() * 10 + myCounter.numbers.back());
+      FloorCounter myCounter { };
+      bool firstValueFound { false };
+      bool secondValueFound { false };
+      uint8_t lowerValue, higherValue { 0 };
+      
+      size_t lengthOfSubstring { 1 };
+      while(lengthOfSubstring <= line.size() and (not firstValueFound)) {
+        std::string testSequence { line.substr( 0,lengthOfSubstring) };
+        for(size_t i = 0; i < numbers.size(); i++) {
+          if(( testSequence.find(charNumbers.at(i)) != std::string::npos) or 
+            (testSequence.find(numbers.at(i)) != std::string::npos)) {
+            std::cout<<"Erster String: " << testSequence << std::endl;
+            firstValueFound = true;
+            lowerValue = i;
+            break;
           }
-          break;
+        }
+        lengthOfSubstring++;
       }
-      //The datafield needs to be cleared for the usage together with the next line s
-      myCounter.numbers.clear();
+
+      lengthOfSubstring = 1;
+      while (lengthOfSubstring <= line.size() and firstValueFound and (not secondValueFound)) {
+        std::string testSequence { line.substr(line.size()-lengthOfSubstring) };
+        for(size_t i = 0; i < charNumbers.size(); i++) {
+          if((testSequence.find(charNumbers.at(i)) != std::string::npos) or
+           (testSequence.find(numbers.at(i)) != std::string::npos)) {
+            std::cout<<"Zweiter String: " << testSequence << std::endl;
+            secondValueFound = true;
+            higherValue = i;
+            break;
+          }
+        }
+        lengthOfSubstring++;
+      }
+      size_t valueToAdd { 0 };
+      if(firstValueFound and not secondValueFound) {
+        valueToAdd += lowerValue * 10 + lowerValue;
+      }
+      else if (firstValueFound and secondValueFound) {
+        valueToAdd += lowerValue * 10 + higherValue;
+      }
+      totalValue += valueToAdd;
+      std::cout<<"Zwischenwert:"<<valueToAdd<<std::endl;
     }
-    std::cout << "The sum of the decrypter numbers is: " << myCounter.total;
-     
   }
-  else {
-    std::cerr << "File could not be opened!" << std::endl;
-  }
+  std::cout << "The sum of the decrypter numbers is: " << totalValue;
   return 0;
 }
